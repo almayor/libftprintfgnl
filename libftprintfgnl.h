7cda6abc31893914f0845f23dbbf35c2317ba031
almayor <mayorovme@gmail.com>
2020-05-11 16:35:32 +0300

    norme

--- libftprintfgnl.h
+++ libftprintfgnl.h
@@ -6,15 +6,16 @@
 /*   By: unite <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/10 22:02:07 by unite             #+#    #+#             */
-/*   Updated: 2020/05/10 22:17:43 by unite            ###   ########.fr       */
+/*   Updated: 2020/05/11 16:35:16 by unite            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFTPRINTFGNL
-# define LIBFTPRINTFGNL
+#ifndef LIBFTPRINTFGNL_H
 
-#include "libft/libft.h"
-#include "get_next_line/get_next_line.h"
-#include "ft_printf/ft_printf.h"
+# define LIBFTPRINTFGNL_H
+
+# include "libft/libft.h"
+# include "get_next_line/get_next_line.h"
+# include "ft_printf/ft_printf.h"
 
 #endif
